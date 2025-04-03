5205d128bff8fd6084c54daceb5e5f89e66b8366
NycollySena <nycolly.sena@dcomp.ufs.br>
2025-04-03 19:36:24 -0300

    embaralhar as cartas e renderizar

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ const cartas = [
     { name: "D", img: "D"},
     { name: "D", img: "D"},
     ];
+     //EMBARALHAR AS CARTAS
+     cartas.sort(() => 0.5 - Math.random());
+
+    
+     //RENDERIZANDO AS CARTAS
+     
+     const jogo = document.querySelector("#jogo");
+ 
+     const cartasElements = cartas.map(carta => {
+         const cartaElement = document.createElement("div");
+         cartaElement.classList.add("carta");
+         cartaElement.dataset.name = carta.name;
+         cartaElement.innerText = "?";
+          return cartaElement
+     });
+ //O unico jeito q encontrei de adicionar os elementos no documento usando foreach:(
+    cartasElements.forEach(cartaElement => {
+     jogo.appendChild(cartaElement);
+    });
+    
+   
