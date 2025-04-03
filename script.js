6426b2cd01c810152b866403c12877945fa20e96
emanuellcmr <emanuelarmy2@gmail.com>
2025-04-02 23:34:21 -0300

    adicionado script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const cartas = [
+    { name: "A", img: "A"},
+    { name: "A", img: "A"},
+    { name: "B", img: "B"},
+    { name: "B", img: "B"},
+    { name: "C", img: "C"},
+    { name: "C", img: "C"},
+    { name: "D", img: "D"},
+    { name: "D", img: "D"},
+    ];
